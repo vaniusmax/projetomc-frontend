3148d5ecab81ce3d351c80c23885f9620b4529ce
Vanius Silva Oliveira <vaniusmax@gmail.com>
2018-06-25 22:31:45 -0300

    Carregando produtos de uma determinada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/cliente.service';
+import { ProdutoService } from '../services/domain/produtos.service';
 
 
 
@@ -38,7 +39,8 @@ import { ClienteService } from '../services/cliente.service';
     ErrorInterceptorProvider,
     AuthService,
     StorageService,
-    ClienteService
+    ClienteService,
+    ProdutoService
     
   ]
 })
