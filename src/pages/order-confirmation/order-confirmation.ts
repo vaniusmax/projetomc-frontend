b88de51c7d704a2a3e2af1aa33c229a4bccc0649
Vanius Silva Oliveira <vaniusmax@gmail.com>
2018-07-16 22:43:08 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController
             , public navParams: NavParams
@@ -53,7 +54,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
         .subscribe(response => {
           this.cartService.createOrClearCart();
-          console.log(response.headers.get('location'));
+          this.codPedido = this.extractId(response.headers.get('location'));
         }, error => {
            if(error.status == 403){
              this.navCtrl.setRoot('HomePage');
@@ -63,5 +64,14 @@ export class OrderConfirmationPage {
 
   back(){
     this.navCtrl.setRoot('CartPage')
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage')
+  }
+
+  private extractId(location: string): string{
+    let positon = location.lastIndexOf('/');
+    return location.substring(positon + 1, location.length);
   }
 }
