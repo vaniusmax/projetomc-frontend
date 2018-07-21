1e3a1ac741298d0f472e45b59e6e0a0f60b503e8
vaniusmax <38790073+vaniusmax@users.noreply.github.com>
2018-07-21 11:05:16 -0300

    Usando cordova para tirar foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CameraOptions, Camera } from '../../../node_modules/@ionic-native/camera';
 
 /**
  * Generated class for the ProfilePage page.
@@ -20,11 +21,14 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController
             , public navParams: NavParams
             , public storage: StorageService
-            , public clienteService: ClienteService) {
+            , public clienteService: ClienteService
+            , public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -50,6 +54,27 @@ export class ProfilePage {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
     }, error => {
      
+    });
+  }
+
+  getCameraPicture(){
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+     // Handle error
     });
   }
 
