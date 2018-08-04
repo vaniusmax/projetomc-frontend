54cef3c32b441902e92a1d96c1b4e563814dca6e
vaniusmax <38790073+vaniusmax@users.noreply.github.com>
2018-08-04 09:52:50 -0300

    cancelamento botão da camera bugfix

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -78,6 +78,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
      // Handle error
     });
   }
@@ -100,6 +101,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
      // Handle error
     });
   }
